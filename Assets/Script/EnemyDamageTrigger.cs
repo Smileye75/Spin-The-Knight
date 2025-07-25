be12b8924ec942deefad8530d1138bddf755e729
Smileye <empty0smile775@gmail.com>
2025-07-25 19:20:24 +0400

    Testing the First Boss (Goblin Shaman)

--- Assets/Script/EnemyDamageTrigger.cs
+++ Assets/Script/EnemyDamageTrigger.cs
@@ -11,8 +11,6 @@ public class EnemyDamageTrigger : MonoBehaviour
         // Check if the player was hit
         if (!other.CompareTag("Player")) return;
 
-        if (other.CompareTag("PlayerStomp")) return;
-
         if (other.TryGetComponent<HealthSystem>(out HealthSystem playerHealth))
         {
             playerHealth.DamageManager(damageAmount);
